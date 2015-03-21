2e9b1c92dbdac125b86ff8119c8152058d2dbaf1
corbin <icorbino91@gmail.com>
2015-03-21 12:38:14 -0500

    test commit.

--- MessagingTutorialSkeleton/src/main/java/com/sinch/messagingtutorialskeleton/LoginActivity.java
+++ MessagingTutorialSkeleton/src/main/java/com/sinch/messagingtutorialskeleton/LoginActivity.java
@@ -12,6 +12,7 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //#test
     }
 
 }
